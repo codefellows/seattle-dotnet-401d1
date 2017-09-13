4afda676b8e560c7bc3f63874e41f1993d7e7598
Amanda Iverson <amanda@codefellows.com>
2017-09-13 16:22:57 -0700

    Adding class demos

---
+++ Class-03/Class_Demo3/Class_Demo3/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+//using System.Text;
+
+namespace Class_Demo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string filePath = @"C:\Users\amanda\Desktop\CodeFellows\Demo3\Demo3File.txt";
+
+
+                if (!File.Exists(filePath))
+                {
+                    using (FileStream fs = File.Create(filePath))
+                    {
+                        // Using System.Text directly other than thru a using because....
+                        Byte[] myText = new System.Text.UTF8Encoding(true).GetBytes("This is my text");
+                        fs.Write(myText, 0, myText.Length);
+                    }
+                }
+                else
+                {
+                    using (StreamReader sr = File.OpenText(filePath))
+                    {
+                        //string s = "";
+                        //while ((s = sr.ReadLine()) != null)
+                        //{
+                        //    Console.WriteLine(s);
+                        //}
+
+                        string[] words = File.ReadAllLines(filePath);
+
+                        int length = words.Length;
+                        foreach (string line in words)
+                        {
+                            Console.WriteLine(line);
+                        }
+
+                       // Console.Read();
+
+                    }
+
+                    AddText(filePath);
+                    DeleteText(filePath);
+                }
+
+
+
+            }
+            catch (DirectoryNotFoundException)
+            {
+
+                Console.WriteLine("The Directory you suggested does not exist");
+            }
+
+            // using
+        }
+
+        // x is the filepath
+        static void AddText(string filePath)
+        {
+            using (StreamWriter sw = File.AppendText(filePath))
+            {
+                sw.Write(Environment.NewLine);
+                sw.WriteLine("More Text!");
+            }
+
+        }
+
+        static void DeleteText(string filePath)
+        {
+            File.Delete(filePath);
+            Console.WriteLine("your file has been deleted");
+            Console.Read();
+        }
+    }
+}
