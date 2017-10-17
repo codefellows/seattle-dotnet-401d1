4688c5434014b4269909025c69245ad66d6ee66d
amanda <ajohnson764@gmail.com>
2017-10-16 20:17:02 -0700

    Adding Class Demo

---
+++ Class26/Class26Demo/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Class26Demo.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Class26Demo.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
